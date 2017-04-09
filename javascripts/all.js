e8713c51e8716570c041733844b85b402aaa68f0
Mateo Murphy <mateo.murphy@gmail.com>
2017-04-09 13:50:57 -0400

    Initial v2

---
+++ javascripts/all.js
@@ -0,0 +1,57 @@
+// Cache selectors
+var lastId,
+    topMenu = $(".menu"),
+
+    // All list items
+    menuItems = topMenu.find("a"),
+
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = $(href).position().top,
+      currentTop = $('.content').scrollTop();
+
+  $('.content').stop().animate({
+      scrollTop: currentTop + offsetTop
+  }, 300);
+
+  e.preventDefault();
+});
+
+// Bind to scroll
+$('.content').scroll(function(){
+
+   // Get container scroll position
+   var fromTop = $(this).scrollTop();
+
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).position().top <= 100) {
+       return this;
+     }
+   });
+
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+
+   if (lastId !== id) {
+       lastId = id;
+
+       // Set/remove active class
+       menuItems
+         .parent()
+         .removeClass("active")
+         .end()
+         .filter("[href='#"+id+"']")
+         .parent()
+         .addClass("active");
+   }
+});
